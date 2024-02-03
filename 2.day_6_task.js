a386b1f954d73e6a43a19bff9152827b9982159f
Aditya <133390310+adityasanap24@users.noreply.github.com>
2024-02-03 22:11:32 +0530

    Add files via upload
    guvi javascript task 
    task no 2. make a class of circle using UML diagram

---
+++ 2.day_6_task.js
@@ -0,0 +1,33 @@
+//2. class-circle
+class Circle{
+    constructor(radius,color){
+        this.radius = radius;
+        this.color = color;
+    }
+
+    getRadius(){
+        console.log(`Radius of Circle is: ${this.radius}`)
+    }
+
+    getColor(){
+        console.log(`Color of Cicle is: ${this.color}`)
+    }
+
+    getArea(){
+        const pi = 3.14;
+        console.log(`Area of Circle is: ${pi*(this.radius*this.radius)}`)
+    }
+
+    getCircumference(){
+        const pi = 3.14;
+        console.log(`Circumference of circle is: ${2*pi*this.radius}`)
+    }
+
+}
+
+let circle = new Circle(2.0,"black");
+
+circle.getRadius();
+circle.getColor();
+circle.getArea();
+circle.getCircumference();
